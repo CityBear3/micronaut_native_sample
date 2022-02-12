37e2a8ef7205b599a406e25b76f71738284d8f37
CityBear3 <68993998+CityBear3@users.noreply.github.com>
2022-02-12 09:06:33 +0900

    change: removed response code and added status code

--- src/main/kotlin/com/example/api/response/SignupResponse.kt
+++ src/main/kotlin/com/example/api/response/SignupResponse.kt
@@ -4,4 +4,4 @@ import io.micronaut.core.annotation.Introspected
 import java.util.*
 
 @Introspected
-data class SignupResponse(val code: Int, val userID: UUID)
+data class SignupResponse(val status: Int, val userID: UUID)

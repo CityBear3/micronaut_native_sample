d779db542b1a1984190b99992480179d59628671
CityBear3 <68993998+CityBear3@users.noreply.github.com>
2022-02-12 10:04:15 +0900

    feat: add error handling

--- src/main/kotlin/com/example/api/controller/UserController.kt
+++ src/main/kotlin/com/example/api/controller/UserController.kt
@@ -4,7 +4,9 @@ import com.example.api.request.SignupRequest
 import com.example.api.response.SignupResponse
 import com.example.application.service.UserApplicationService
 import com.example.domain.user.*
+import com.example.exception.MissingRequiredParameterException
 import io.micronaut.http.HttpResponse
+import io.micronaut.http.HttpStatus
 import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Consumes
@@ -23,17 +25,21 @@ class UserController(
     @Secured(SecurityRule.IS_ANONYMOUS)
     @Consumes(MediaType.APPLICATION_JSON)
     @Post("/signup")
-    fun signup(@Body signupRequest: SignupRequest): Publisher<HttpResponse<SignupResponse>> {
+    fun signup(@Body signupRequest: SignupRequest?): Publisher<HttpResponse<SignupResponse>> {
         return Flux.create({emitter: FluxSink<HttpResponse<SignupResponse>> ->
-            val user = User(
-                UserName(signupRequest.name),
-                UserEmail(signupRequest.email),
-                UserPassword(signupRequest.password),
-                UserRole.ROLE_USER
-            )
-            val createdUser = userApplicationService.signup(user)
-            emitter.next(HttpResponse.created(SignupResponse(code = 1201, userID = createdUser.userId!!)))
-            emitter.complete()
+            if (signupRequest != null) {
+                val user = User(
+                    UserName(signupRequest.name),
+                    UserEmail(signupRequest.email),
+                    UserPassword(signupRequest.password),
+                    UserRole.ROLE_USER
+                )
+                val createdUser = userApplicationService.signup(user)
+                emitter.next(HttpResponse.created(SignupResponse(HttpStatus.OK.code, createdUser.userId!!)))
+                emitter.complete()
+            } else {
+                emitter.error(MissingRequiredParameterException())
+            }
         }, FluxSink.OverflowStrategy.ERROR)
     }
 }

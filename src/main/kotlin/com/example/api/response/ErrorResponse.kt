347f790e2900efb6f4650d9c351916b38930f360
CityBear3 <68993998+CityBear3@users.noreply.github.com>
2022-02-12 09:02:38 +0900

    change: changed error code to String and added status code

--- src/main/kotlin/com/example/api/response/ErrorResponse.kt
+++ src/main/kotlin/com/example/api/response/ErrorResponse.kt
@@ -3,4 +3,4 @@ package com.example.api.response
 import io.micronaut.core.annotation.Introspected
 
 @Introspected
-data class ErrorResponse(val code: Int, val message: String)
+data class ErrorResponse(val status: Int, val code: String, val message: String)
